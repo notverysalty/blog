2bb0a2b479be3ea8a4dee6200ea656b52567b884
Yy <1197181269@qq.com>
2021-08-25 11:00:06 +0800

    修改端口

--- blog_server/models/db/dal.js
+++ blog_server/models/db/dal.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/blog', (err) => {
+mongoose.connect('mongodb://127.0.0.1:27017/blog', (err) => {
     if (err) {
         console.log('数据库连接失败！错误原因：' + err)
     } else {
